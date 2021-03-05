d54fa2c64c035f26f8910a892a1ee9a2f12f4742
SGKwon95 <ssunder7@naver.com>
2021-03-05 14:18:40 +0900

    complete

--- src/kotlinLotto.kt
+++ src/kotlinLotto.kt
@@ -1,12 +1,18 @@
+import java.util.Scanner
 fun main() {
-    //랜덤 번호 8개 뽑기
-    val nums = Array(8,{0})
-    val picked = Array<Boolean>(46,{false})
-    for (i in 0..7) {
-        do {
-            nums[i] = (1..45).random()
-        } while(picked[nums[i]])
-        picked[nums[i]] = true
+    print("시도할 횟수 입력 : ")
+    val numofPick = Scanner(System.`in`).nextInt()
+    for (T in 1..numofPick) {
+        println("===== ${T}번째 시도 =====")
+        val nums = Array<Int>(7,{0})
+        val picked = Array<Boolean>(46,{false})
+        for (i in 0..6) {
+            do {
+                nums[i] = (1..45).random()
+            } while(picked[nums[i]])
+            picked[nums[i]] = true
+        }
+        nums.forEachIndexed { index, i -> println("${index+1} 번째 숫자 : ${i}") }
+        println()
     }
-    nums.forEachIndexed { index, i -> println("${index+1} 번째 숫자 : ${i}") }
 }
